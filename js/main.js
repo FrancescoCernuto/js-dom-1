cbb8a65908f32b442eee8f9d508e31ad718ea3fe
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-10-22 16:12:58 +0200

    add-attributes-forlampEl

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ const lampEl = document.querySelector("#lamp");
 const toggleLampEl = document.querySelector("#toggleLamp");
 
 toggleLampEl.addEventListener("click", () => {
+    lampEl.attributes.src.value === "./img/white_lamp.png"
+    ? (lampEl.attributes.src.value = "./img/yellow_lamp.png")
+    : (lampEl.attributes.src.value = "./img/white_lamp.png");
